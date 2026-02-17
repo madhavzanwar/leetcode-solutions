47a391275ead8a0bc28e680a38815a3721d86b4e
Madhav <madhavzanwar13@gmail.com>
2026-02-18 00:38:09 +0530

    Add valid_anagram.py with isAnagram function

---
+++ Arrays/valid_anagram.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        a = sorted(s)
+        b = sorted(t)
+
+        if a == b:
+            return True
+        else:
+            return False
