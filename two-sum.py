0f85a616cad16dc539b0b4585714425877efb41d
Madhav <madhavzanwar13@gmail.com>
2026-02-19 15:26:18 +0530

    Delete two-sum.py

--- two-sum.py
+++
@@ -1,9 +0,0 @@
-# Problem: Two Sum
-# Approach: Brute Force
-
-class Solution(object):
-    def twoSum(self, nums, target):
-        for i in range(len(nums)):
-            for j in range(i+1, len(nums)):
-                if nums[i] + nums[j] == target:
-                    return [i, j]
