5f328bff87642ff3ca761e12220f954970189cf3
Madhav <madhavzanwar13@gmail.com>
2026-02-24 23:52:46 +0530

    Time: 0 ms (100%), Space: 12.4 MB (58.95%) - LeetHub

--- 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.py
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.py
@@ -4,4 +4,7 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
+        #Sum of first n odd numbers = n²
+        #Sum of first n even numbers = n(n+1)
+        #GCD(n² , n(n+1))
         return n
