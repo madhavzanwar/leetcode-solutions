14d7b2f47d99a13b58c3c846387a2d01dfa8e19a
Madhav <madhavzanwar13@gmail.com>
2026-02-27 12:38:46 +0530

    Time: 0 ms (100%), Space: 12.6 MB (28.82%) - LeetHub

---
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def maximumWealth(self, accounts):
+        """
+        :type accounts: List[List[int]]
+        :rtype: int
+        """
+        return max(sum(acc) for acc in accounts)
+     
+            
+            
