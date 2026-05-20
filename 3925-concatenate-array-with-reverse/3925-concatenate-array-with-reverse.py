e3faaceae1df2fd77d497ef9d326c3ab5b41f8f7
Madhav <madhavzanwar13@gmail.com>
2026-05-20 06:26:18 +0530

    Time: 0 ms (100%), Space: 12.3 MB (79.07%) - LeetHub

---
+++ 3925-concatenate-array-with-reverse/3925-concatenate-array-with-reverse.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def concatWithReverse(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        
+        ans = nums[::-1]
+        return nums+ans
