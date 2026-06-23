f5449216f29c2a64217de378123fb2f9b790fca4
Madhav <madhavzanwar13@gmail.com>
2026-06-24 00:46:08 +0530

    Time: 0 ms (100%), Space: 8.8 MB (72.11%) - LeetHub

---
+++ 0290-word-pattern/0290-word-pattern.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    bool wordPattern(string pattern, string s) {
+        vector<string> words;
+        string word;
+        stringstream ss(s);
+
+        while (ss >> word) {
+            words.push_back(word);
+        }
+
+        if (pattern.length() != words.size()) {
+            return false;
+        }
+
+        unordered_map<char, string> charToWord;
+        unordered_map<string, char> wordToChar;
+
+        for (int i = 0; i < pattern.length(); i++) {
+            char ch = pattern[i];
+            string w = words[i];
+
+            if (charToWord.count(ch)) {
+                if (charToWord[ch] != w) {
+                    return false;
+                }
+            } else {
+                charToWord[ch] = w;
+            }
+
+            if (wordToChar.count(w)) {
+                if (wordToChar[w] != ch) {
+                    return false;
+                }
+            } else {
+                wordToChar[w] = ch;
+            }
+        }
+
+        return true;
+    }
+};
