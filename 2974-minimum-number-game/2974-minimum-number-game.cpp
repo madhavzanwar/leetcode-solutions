16724af4fa4213971aeb897fcda04d9ab4f60975
Madhav <madhavzanwar13@gmail.com>
2026-07-09 13:28:52 +0530

    Time: 2 ms (27.82%), Space: 28.3 MB (31.83%) - LeetHub

---
+++ 2974-minimum-number-game/2974-minimum-number-game.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> numberGame(vector<int>& nums) {
+        vector<int>arr;
+
+        //sort and swap adjecent elements
+        sort(nums.begin(), nums.end());
+
+        for(int i = 0; i<nums.size() - 1; i+=2){ //i+=2 
+            arr.push_back(nums[i+1]);
+            arr.push_back(nums[i]);
+        }
+        return arr;
+        
+    }
+};
