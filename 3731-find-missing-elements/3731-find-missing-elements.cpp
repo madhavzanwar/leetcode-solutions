695a4e22b8bcf05e28670cbed7135c06c3c40abe
Madhav Zanwar <madhavzanwar13@gmail.com>
2026-08-04 08:39:05 +0530

    Time: 4 ms (35.49%), Space: 32.2 MB (91.09%) - LeetHub

--- 3731-find-missing-elements/3731-find-missing-elements.cpp
+++ 3731-find-missing-elements/3731-find-missing-elements.cpp
@@ -4,7 +4,7 @@ public:
         sort(nums.begin(), nums.end());
         vector<int>result;
         for(int i = 0; i<nums.size()-1; i++){
-            while(nums[i]+1 <  nums[i+1]){
+            while(nums[i]+1 < nums[i+1]){
                 nums[i]++;
                 result.push_back(nums[i]);
             }
