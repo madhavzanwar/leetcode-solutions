03d245181a51fbb15aa2ec597d1d02577d65ef9f
Madhav Zanwar <madhavzanwar13@gmail.com>
2026-08-10 19:14:00 +0530

    Time: 12 ms (10.15%), Space: 127.6 MB (18.88%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -10,39 +10,19 @@
  */
 class Solution {
 public:
-    ListNode* reverseLinkedList(ListNode* head) {
-        if (head == NULL || head->next == NULL) return head;
-        ListNode* newHead = reverseLinkedList(head->next);
-        ListNode* front = head->next;
-        front->next = head;
-        head->next = NULL;
-        return newHead;
-    }
-
     bool isPalindrome(ListNode* head) {
-        if (head == NULL || head->next == NULL) return true;
-
-        ListNode* slow = head;
-        ListNode* fast = head;
-        while (fast->next != NULL && fast->next->next != NULL) {
-            slow = slow->next;
-            fast = fast->next->next;
+        ListNode* temp = head;
+        stack<int>st;
+        while(temp!=NULL){
+            st.push(temp->val);
+            temp = temp->next;
         }
-
-        ListNode* newHead = reverseLinkedList(slow->next);
-        ListNode* first = head;
-        ListNode* second = newHead;
-        bool ans = true;
-        while (second != NULL) {
-            if (first->val != second->val) {
-                ans = false;
-                break;
-            }
-            first = first->next;
-            second = second->next;
+        temp = head;
+        while(temp!=NULL){
+            if(st.top()!=temp->val) return false;
+            temp = temp->next;
+            st.pop();
         }
-
-        slow->next = reverseLinkedList(newHead);
-        return ans;
+        return true;
     }
 };
