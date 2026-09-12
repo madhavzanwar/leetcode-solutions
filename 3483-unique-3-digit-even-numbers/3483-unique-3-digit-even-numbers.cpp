13f62bbbd000b6ef15ac1e4e9799b785a48013e7
Madhav Zanwar <madhavzanwar13@gmail.com>
2026-09-12 16:30:32 +0530

    Time: 7 ms (65.65%), Space: 30.9 MB (51.58%) - LeetHub

--- 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.cpp
+++ 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.cpp
@@ -1,23 +1,27 @@
 class Solution {
 public:
     int totalNumbers(vector<int>& digits) {
-        unordered_set<int> seen;
+        set<int> validNumbers;
         int n = digits.size();
 
-        for(int h = 0; h < n; h++){
-            if (digits[h] == 0) continue;
-            for(int t = 0; t < n; t++){
-                if (t == h) continue;
-                for(int u = 0; u < n; u++){
-                    if(u == h || u == t) continue;
-                    if(digits[u] % 2 != 0) continue;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (j == i) continue;  
+                for (int k = 0; k < n; k++) {
+                    if (k == i || k == j) continue;  
 
-                    int num = digits[h]*100+digits[t]*10+digits[u];
-                    seen.insert(num);
+
+                    int num = 100*digits[i]+10*digits[j]+digits[k];
+                    
+                    if(digits[i]==0) continue;
+                    
+                    if(digits[k] %2 !=0) continue;
+                    
+                    validNumbers.insert(num);
                 }
             }
         }
 
-        return seen.size();
+        return validNumbers.size();
     }
 };
